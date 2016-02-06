27b92f087f5f80864d6e404b8a53253485366db7
ZINGDING <tjgmltn07@naver.com>
2016-02-07 00:34:59 +0900

    0.1-beta-release

---
+++ Source Code/src/Givecookie/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Givecookie;
+
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\Event;
+use pocketmine\block\Block;
+use pocketmine\item\item;
+use pocketmine\item\Cookie;
+class Main extends PluginBase implements Listener{
+	 public function onLoad() {
+	 	 $this->getLogger()->info(" ");
+		$this->getLogger()->info("Givecookie plugin");
+	     $this->getLogger()->info("Made By ZINGDING");
+	}
+		public function onEnable() {
+			 $this->getLogger()->info(" ");
+			 $this->getLogger()->info("Givecookie Plugin on Enable");
+			 $this->getLogger()->info(" ");
+			 $this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+		 }
+		 
+		 public function onPlayerMove(PlayerMoveEvent $event) {
+		 $block = $event->getPlayer()->getLevel()->getBlock($event->getPlayer()->getPosition()->subtract(0,1,0));
+	      if($block->getId() === 35 && $block->getDamage() ===1 ) {
+	                 $item = 
+                   	 $event->getPlayer ()->additem ($item);
+	      }
+
+    
+        }
+}
+
+?>
