a3a266d41c34df663f4d87404392c8a07fb38e53
DeBe <huu6677@naver.com>
2016-02-09 15:44:44 +0900

    Fix blockHash error?
    IDK

--- Source Code/src/Givecookie/Main.php
+++ Source Code/src/Givecookie/Main.php
@@ -6,34 +6,25 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
-use pocketmine\event\Event;
-use pocketmine\block\Block;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener{
-	 public function onLoad() {
-		$this->getLogger()->info("This plugin is Givecookie plugin.");
-	    $this->getLogger()->info("Made By ZINGDING.");
+	public function onLoad() {
+		$this->getLogger()->info("Givecookie plugin.");
+		$this->getLogger()->info("Made By ZINGDING.");
 	}
-		public function onEnable() {
-			 $this->getLogger()->info("Givecookie plugin enabled.");
-			 $this->getLogger()->info("This plugin's license is the ZINGDING-EULA.");
-			 $this->getServer()->getPluginManager()->registerEvents($this,$this);
-		 }
-		 
-		 	public function onPlayerMove(PlayerMoveEvent $event) {
-		 	$block = $event->getPlayer()->getLevel()->getBlock($event->getPlayer()->getPosition()->subtract(0,1,0));
-		    if($block->getId() == 35 && $block->getDamage() ==1 ) {
-		    $player = $event->getPlayer ();
-		 	$player->getInventory()->addItem(new Item(ITEM::COOKIE, 0, 1 ));
-		    }
-		    {$slot = $event->getPlayer->getInventory()->getItemInHand();
-		 	if( $slot->getID() == ITEM::COOKIE )
-            $player->sendTip(TextFormat::GRAY . ">" . TextFormat::GREEN . "You've recieved a cookie!");
-		 	
-	                
-	      }
-        }
-      }
-
+	public function onEnable() {
+		$this->getLogger()->info("Givecookie plugin enabled.");
+		$this->getLogger()->info("This plugin's license is the ZINGDING-EULA.");
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	}
+	public function onPlayerMove(PlayerMoveEvent $event) {
+		$block = $event->getPlayer()->getLevel()->getBlock($event->getPlayer()->subtract(0,1,0)->floor());
+		if($block->getId() === 35 && $block->getDamage() === 1) {
+			$player = $event->getPlayer();
+			$player->getInventory()->addItem(new Item(ITEM::COOKIE, 0, 1 ));
+			$player->sendTip(TextFormat::GRAY . ">" . TextFormat::GREEN . "You've recieved a cookie!");
+		}
+	}
+}
 ?>
