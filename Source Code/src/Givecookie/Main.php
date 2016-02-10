7b7ca6b4dfc8f539c84651fa26b8f1344232f24f
ZINGDING <tjgmltn07@naver.com>
2016-02-10 22:50:58 +0900

    v5.0-Release

--- Source Code/src/Givecookie/Main.php
+++ Source Code/src/Givecookie/Main.php
@@ -2,13 +2,12 @@
 
 namespace Givecookie;
 
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\network\protocol\PlayerActionPacket;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
 use pocketmine\utils\TextFormat;
-use pocketmine\math\Vector3;
+
 
 class Main extends PluginBase implements Listener{
 	public function onLoad() {
@@ -16,16 +15,20 @@ class Main extends PluginBase implements Listener{
 		$this->getLogger()->info("§cMade By ZINGDING.");
 	}
 	public function onEnable() {
-		$this->getLogger()->info("§dGivecookie plugin enabled.");
+		$this->getLogger()->info("§dGiveCookie Plugin enabled.");
 		$this->getLogger()->info("§cThis plugin's License is the ZINGDING-EULA.");
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 	}
-	public function onPlayerMove(PlayerMoveEvent $event) {
-		$player = $event->getPlayer();
-		$block = $player->level->getBlock(new Vector3((int) $player->x, (int) ($player->y - 1), (int) $player->z));
-		if($block->getId() === 35 && $block->getDamage() === 1) {
-			$player->getInventory()->addItem(new Item(ITEM::COOKIE, 0, 1));
-			$player->sendTip(TextFormat::GRAY . "{§dGiveCookie}" . TextFormat::GREEN . "You've received a cookie!");
+	
+	public function onDisable() {
+	$this->getLogger()->emergency("§dGiveCookie Plugin is Disabled.");
+		}
+	
+	public function playerInteract(PlayerInteractEvent $event) {
+		if($event->getBlock ()->getId () == 35 && $event->getBlock ()->getDamage () == 1) {
+		$player = $event->getPlayer();	
+		$player->getInventory()->addItem(new Item(ITEM::COOKIE, 0, 1));
+	    $player->sendTip(TextFormat::GRAY . "{§dGiveCookie}" . TextFormat::GREEN . "You've received a cookie!");
 		}
 	}
 }
